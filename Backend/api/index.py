1709b9e64daf4ce420b11d228315021e8ffc0667
Tainted <161955641+usmank06@users.noreply.github.com>
2024-11-16 22:43:38 -0600

    Initial Endpoint Structure

--- Backend/api/index.py
+++ Backend/api/index.py
@@ -1,11 +1,46 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+import rag
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+history = {}
 
-@app.route('/about')
-def about():
-    return 'About'
+global counter
+counter = 0
+
+
+@app.route('/postMessage', methods=['POST'])
+def post_message():
+    data = request.json
+    message = data.get('message')
+    convo_id = data.get('convoId', None)
+
+    # Add message to history
+    if convo_id is not None:
+        messages = history[convo_id]
+        messages.append(message)
+        history[convo_id].append(message)
+    else:
+        messages = [message]
+
+    reply = rag.get_response(messages)
+    suggestions = [rag.get_suggesstions(message)]
+
+    # Create response
+    response = {
+        "reply": reply,
+    }
+    if suggestions:
+        response["suggestions"] = suggestions
+
+    # Include convoId only for the first response
+    if convo_id is None:
+        global counter
+        counter += 1
+        history[str(counter)] = messages
+        response["convoId"] = str(counter)
+
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
